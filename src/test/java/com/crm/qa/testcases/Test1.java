353b6c543a66db193d27d2cccd13c774cf1266e8
Test Automation <my.test.automation777@gmail.com>
2022-01-05 01:06:32 +0530

    Test Update

---
+++ src/test/java/com/crm/qa/testcases/Test1.java
@@ -0,0 +1,38 @@
+package com.crm.qa.testcases;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Test;
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+public class Test1 extends TestBase{
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+//		driver.get("https://www.freecrm.com/index.html");
+	}
+
+	@Test
+	public void sample(){
+		driver.get("www.javatpoint.com");		
+	}
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+}
